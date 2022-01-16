a6263af50c708d20434e8d95f495acbd85059949
Ruben Cerqueira pg47626@alunos.uminho.pt <pg47626@search7edu.local>
2022-01-16 08:28:26 +0000

    Paralel coiso

--- teste.sh
+++ teste.sh
@@ -2,13 +2,13 @@
 #SBATCH --time=1:00 
 #SBATCH --partition=cpar
 
-MY=/home/pg47576/CP2021
+MY=/home/pg47626/CP2021
 
 module load papi/5.4.1
 export OMP_NUM_THREADS=16
-$MY/test 10000 16 
-time  $MY/test 10000 16
+$MY/test 1000000 100 
+time  $MY/test 1000000 100
 
 
-perf record  ./test
-perf report -n > perfreport 
+# perf record  ./test
+# perf report -n > perfreport 
